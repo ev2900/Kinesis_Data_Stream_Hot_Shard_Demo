0490a4f72ee0c37e35a57c70a38b5f336853f494
Christopher Sharkey <ev2900@gmail.com>
2022-06-01 19:11:33 -0500

    Update send_data_to_a_shard.py

--- Hot Shard/Sample Data Producer/send_data_to_a_shard.py
+++ Hot Shard/Sample Data Producer/send_data_to_a_shard.py
@@ -31,9 +31,10 @@ for message in range(0, numberOfMessagesToSend):
 		StreamName = kinesisDataStreamName,
 		Data = json.dumps(messageBody),
 		PartitionKey = str(hash(range(0,1))),
-		# ExplicitHashKey = "1" # shard 0
+		ExplicitHashKey = "1" # shard 0
 		# ExplicitHashKey = "113427455640312821154458202477256070486" # shard 1
 		# ExplicitHashKey = "226854911280625642308916404954512140971" # shard 2
+		# ...
 	)
 
 	print("Message # " + str(counter) + " sent to " + str(response['ShardId']))
